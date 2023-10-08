db0ef24c889eca98d6c1fbf51249e02f7b044f03
Nikita Ochinskiy <n.ochinskiy@autodoc.eu>
2023-10-09 00:02:02 +0300

    First commit

---
+++ master.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+msgs = []
+
+secondaries = ["http://secondary1:5000", "http://secondary2:5000"]
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/append', methods=['POST'])
+def append():
+    # Get message
+    new_msg = request.get_json().get('message')
+
+    # POST it into each secondaries
+    for sec in secondaries:
+        print(sec)
+        # Check each request on succeed work
+        # return requests.get(sec + '/msgs_list')
+        response = requests.post(sec + '/append', json={'message': new_msg})
+        if response.json().get("message") != "ACK":
+            raise requests.RequestException("Do not receive an acknowledgment")
+
+    # Post message in master after succeed work
+    msgs.append(new_msg)
+    return jsonify({"message": "Message was created"}), 201
+
+
+@app.route('/msgs_list', methods=['GET'])
+def msgs_list():
+    return jsonify({'list': msgs})
+
+
+@app.errorhandler(requests.RequestException)
+def handle_request_exception(error):
+    return jsonify({"error": str(error)}), 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ secondary.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+import sys
+
+app = Flask(__name__)
+
+msgs = []
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/append', methods=['POST'])
+def append():
+    new_msg = request.get_json().get('message')
+    msgs.append(new_msg)
+    return jsonify({"message": "ACK"}), 201
+
+
+@app.route('/msgs_list', methods=['GET'])
+def msgs_list():
+    return jsonify({'list': msgs})
+
+
+# port = int(sys.argv[1]) if len(sys.argv) > 1 else 5001
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
