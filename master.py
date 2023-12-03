2be0c052fd2d746e2b9f9980c0fdf94d8bca5bc8
Nikita Ochinskiy <n.ochinskiy@autodoc.eu>
2023-12-03 17:03:07 +0200

    Build 3rd iteration

--- master.py
+++ master.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, request, jsonify
 import requests
 import asyncio
@@ -11,17 +13,18 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 
-global message_counter
-message_counter = 0
+app.message_counter = 0
 
-msgs = []
+app.msgs = []
 
 secondaries = [os.environ.get('SECONDARY1_URL'), os.environ.get('SECONDARY2_URL')]
 
 
+HEALTH_CHECK_INTERVAL = 10
+
+
 def increment_counter():
-    global message_counter
-    message_counter += 1
+    app.message_counter += 1
 
 
 @app.route('/append', methods=['POST'])
@@ -41,15 +44,15 @@ async def append():
     # Set Condition for wait write concern replication
     condition = CountDownLatch(count=w, app=app)
 
+    # Add replication on master
+    app.msgs.append((app.message_counter, new_msg))
+    app.logger.debug('Replication for master was ended')
+
     # Replicate it for each secondaries
     for inx, sec in enumerate(secondaries):
         Process(target=replication_on_secondary,
-                args=(sec, (message_counter, new_msg), condition)
+                args=(sec, (app.message_counter, new_msg), condition)
                 ).start()
-
-    # Add replication on master
-    msgs.append((message_counter, new_msg))
-    app.logger.debug('Replication for master was ended')
 
     # Wait for condition
     condition.awaiter()
@@ -61,7 +64,7 @@ async def append():
 
 @app.route('/msgs_list', methods=['GET'])
 def msgs_list():
-    return jsonify({'list': sorted(msgs)})
+    return jsonify({'list': app.msgs})
 
 
 @app.errorhandler(requests.RequestException)
@@ -70,16 +73,71 @@ def handle_request_exception(error):
     return jsonify({"error": str(error)}), 500
 
 
-def replication_on_secondary(host, new_msg, condition):
+def replication_on_secondary(host, new_msg, condition, delay=None, retry=False):
     app.logger.debug('Start replication for {0}'.format(host))
-    response = requests.post(host + '/append', json={'message': new_msg})
-    # Check each request on succeed work
-    if response.status_code != 201:
-        raise requests.RequestException("Do not receive an acknowledgment")
 
-    app.logger.debug('Replication for {0} was ended'.format(host))
-    condition.count_down()
+    if retry:
+        app.logger.debug('Delay equals to {0}'.format(sum(delay)))
+        time.sleep(sum(delay))
+        delay = next_delay(delay)
+
+    try:
+        response = requests.post(host + '/append', json={'message': new_msg})
+
+        # Check each request on succeed work
+        if response.status_code != 201:
+            raise requests.RequestException()
+    except requests.RequestException as e:
+        app.logger.debug(e)
+        replication_on_secondary(host, new_msg, condition, delay=delay, retry=True)
+
+    if not retry:
+        app.logger.debug('Replication for {0} was ended'.format(host))
+        condition.count_down()
+
+
+def next_delay(delay):
+    if delay is None:
+        delay = [0, 1]
+
+    buffer = delay[1]
+    delay[1] = sum(delay)
+    delay[0] = buffer
+
+    return delay
+
+
+def check_health(host, delay=None, retry=False):
+    delay = next_delay(delay)
+    time.sleep(sum(delay))
+
+    # Turn off retries in requests library in order check only my retries
+    session = requests.Session()
+    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=0))
+    session.mount('https://', requests.adapters.HTTPAdapter(max_retries=0))
+
+    while True:
+        app.logger.debug(f'Health check with {delay} delay for {host}')
+        try:
+            response = session.get(host + '/health')
+            # Check each request on succeed work
+            if response.status_code != 200:
+                raise requests.RequestException()
+        except requests.RequestException as e:
+            app.logger.debug(e)
+            check_health(host, delay, retry=True)
+
+        if retry:
+            break
+
+        time.sleep(HEALTH_CHECK_INTERVAL)
 
 
 if __name__ == '__main__':
+    for host in secondaries:
+        health_check_thread = multiprocessing.Process(target=check_health,
+                                                      args=(host,)
+                                                      )
+        health_check_thread.start()
+
     app.run(host='0.0.0.0')
